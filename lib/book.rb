e9421283eefe717516a9a28d56181868a39981a5
Ben <benbperry1@gmail.com>
2021-12-15 09:25:20 -0800

    commit tree b9e61582225521ae6b5aed7c5ee2e5fa518bc09f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
+
+
+        # @author = author
+        # @page_count = page_count
+        # @genre = genre
+        # @turn_page = turn_page
